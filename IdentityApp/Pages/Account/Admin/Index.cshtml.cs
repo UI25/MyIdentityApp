17fe015b31a0751042c544b2d8c3a7456e9b94bf
UI <ufuki@outlook.com>
2023-07-09 01:13:49 +0300

    Initial

---
+++ IdentityApp/Pages/Account/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Account.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
